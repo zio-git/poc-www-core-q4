42d35a3a5ebaba32b1e599e52655258024de1a56
DoxDevOps <73119561+DoxDevOps@users.noreply.github.com>
2020-12-16 23:11:59 +0200

    Changing deployment to fit current non-continental setup

--- deploy.py
+++ deploy.py
@@ -18,45 +18,18 @@ for site in xi_api['cluster']:
 
 	if subprocess.call(['ping', param, '1', site['ip']]) == 0:
 
-		# push to site
-		#push_to_site = "rsync " + "-avzhe ssh $PWD/ " + site['user'] + "@" + site['ip'] + ":~/devops/poc"
-		#os.system(push_to_site)
-
-		# unzip nginx
-		#unzip_nginx = "ssh " + site['user'] + "@" + site['ip'] + " 'zcat nginx.gz | docker import - nginx'"
-		#os.system(unzip_nginx)
-
-		# unzip bht_emr_api
-		#unzip_bht_emr_api = "ssh " + site['user'] + "@" + site['ip'] + " 'zcat bht_emr_api.gz | docker import - bht_emr_api'"
-		#os.system(unzip_bht_emr_api)
-
-		# unzip bht_emr_api
-		#unzip_nlims_data_syncroniser = "ssh " + site['user'] + "@" + site['ip'] + " 'zcat nlims_data_syncroniser.gz | docker import - nlims_data_syncroniser'"
-		#os.system(unzip_nlims_data_syncroniser)
-
-# unzip nlims_controller
-		#unzip_nlims_controller = "ssh " + site['user'] + "@" + site['ip'] + " 'zcat nlims_controller.gz | docker import - nlims_controller'"
-		#os.system(unzip_nlims_controller)
-
-		# unzip lab_test_controller
-		#unzip_lab_test_controller = "ssh " + site['user'] + "@" + site['ip'] + " 'zcat lab_test_controller.gz | docker import - lab_test_controller'"
-		#os.system(unzip_lab_test_controller)
-
-		# unzip couchdb
-		#unzip_couchdb = "ssh " + site['user'] + "@" + site['ip'] + " 'zcat couchdb.gz | docker import - couchdb'"
-		#os.system(unzip_couchdb)
-
-		# unzip mysqldb
-		#unzip_mysqldb= "ssh " + site['user'] + "@" + site['ip'] + " 'zcat mysqldb.gz | docker import - mysqldb'"
-		#os.system(unzip_mysqldb)
-
-		# docker compose build
-		#buildx = "ssh " + site['user'] + "@" + site['ip'] + " 'docker-compose build'"
-		#os.system(buildx)
-
-		# docker compose run
-		runx = "ssh " + site['user'] + "@" + site['ip'] + " 'cd ~/devops/poc; docker-compose up -d --no-recreate --remove-orphans'"
-		os.system(runx)
+		# PUSH BHT-EMR-API
+		push_api = "rsync " + "-avzhe ssh $WORKSPACE/BHT-EMR-API/ " + site['user'] + "@" + site['ip'] + ":~/var/www/BHT-EMR-API"
+		os.system(push_api)
+		
+		# PUSH BHT-Core
+		push_core = "rsync " + "-avzhe ssh $WORKSPACE/BHT-Core/ " + site['user'] + "@" + site['ip'] + ":~/var/www/BHT-Core"
+		os.system(push_core)
+		
+		# PUSH ART
+		push_art = "rsync " + "-avzhe ssh $WORKSPACE/BHT-Core/apps/ART/ " + site['user'] + "@" + site['ip'] + ":~/var/www/BHT-Core/apps/ART"
+		os.system(push_art)
+		
 
 		with urllib.request.urlopen('http://10.44.0.52/modules/api/?v=record_sites_deployed&result=1&pipeline_name=Xi-Build-Initiator&sid='+site['id']) as response:
 			html = response.read()
