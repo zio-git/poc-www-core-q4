a702029c66da8c808b93c9771a5129b6b5111e2b
kyryo <owenonions@gmail.com>
2020-12-17 09:51:16 +0200

    Adding shipping functionality

---
+++ ship_core.py
@@ -0,0 +1,30 @@
+import os
+import urllib.request
+import json
+import platform
+import subprocess
+
+# define URL to capture data
+url = 'http://10.44.0.52/modules/api/?v=cluster&pipeline_name=Xi-Build-Initiator'
+req = urllib.request.Request(url)
+
+# parsing response
+r = urllib.request.urlopen(req).read()
+xi_api = json.loads(r.decode('utf-8'))
+
+for site in xi_api['cluster']:	
+
+	param = '-n' if platform.system().lower()=='windows' else '-c'
+
+	if subprocess.call(['ping', param, '1', site['ip']]) == 0:
+
+		# PUSH BHT-EMR-API
+		push_api = "rsync " + "-avzhe ssh $WORKSPACE/BHT-Core/ " + site['user'] + "@" + site['ip'] + ":~/var/www/BHT-Core"
+		os.system(push_api)		
+
+		with urllib.request.urlopen('http://10.44.0.52/modules/api/?v=record_sites_deployed&result=1&pipeline_name=Xi-Build-Initiator&sid='+site['id']) as response:
+			html = response.read()
+			
+	else:
+		with urllib.request.urlopen('http://10.44.0.52/modules/api/?v=record_sites_deployed&result=0&pipeline_name=Xi-Build-Initiator&sid='+site['id']) as response:
+			html = response.read()
