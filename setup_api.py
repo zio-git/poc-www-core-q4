f1caffb80155f3d636d322acd74586ab3500b559
kyryo <owenonions@gmail.com>
2020-12-17 13:08:01 +0200

    Fixing API logic -- migrations

--- setup_api.py
+++ setup_api.py
@@ -18,21 +18,9 @@ for site in xi_api['cluster']:
 
 	if subprocess.call(['ping', param, '1', site['ip']]) == 0:
 
-		# PUSH BHT-EMR-API
-		push_api = "rsync " + "-avzhe --exclude 'config' ssh $WORKSPACE/BHT-EMR-API/ " + site['user'] + "@" + site['ip'] + ":~/var/www/BHT-EMR-API"
-		os.system(push_api)
-
-		# CHECKOUT SPECIFIED VERSION
-		checkout_api = "ssh " + site['user'] + "@" + site['ip'] + " 'cd ~/var/www/BHT-EMR-API; git checkout tags/v4.10.18'"
-		os.system(checkout_api)
-
-		# MIGRATIONS
-		migrations = "ssh " + site['user'] + "@" + site['ip'] + " 'rails db:migrate'"
-		os.system(migrations)
-
-		# METADATA	
-		api_metadata = "ssh " + site['user'] + "@" + site['ip'] + " './bin/update_art_metadata.sh development'"
-		os.system(api_metadata)
+		# SETUP BHT-EMR-API
+		setup_api = "ssh " + site['user'] + "@" + site['ip'] + " 'git checkout tags/v4.10.18'"
+		os.system(setup_api)	
 
 		with urllib.request.urlopen('http://10.44.0.52/modules/api/?v=record_sites_deployed&result=1&pipeline_name=Xi-Build-Initiator&sid='+site['id']) as response:
 			html = response.read()
