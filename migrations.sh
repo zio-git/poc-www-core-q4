3e5879ae3c8da1b30223b9c74fba59535e8745c6
kyryo <owenonions@gmail.com>
2021-01-07 00:18:10 +0200

    Adding PATH for RAKE

--- migrations.sh
+++ migrations.sh
@@ -1,4 +1,5 @@
 #!/bin/bashRun; 
 echo "migrating database....................."
-export PATH=$PATH:$HOME/bin:$HOME/.rvm/gems/ruby-2.5.3/bin:$HOME/.rvm/rubies/ruby-2.5.3/lib/ruby/gems/2.5.0/bin:$HOME/.rvm/rubies/ruby-2.5.3/bin
+export PATH=$PATH:$HOME/bin:$HOME/.rvm/gems/ruby-2.5.3/bin:$HOME/.rvm/rubies/ruby-2.5.3/bin:$HOME/.gem/ruby/2.5.0/bin
+
 rake db:migrate
