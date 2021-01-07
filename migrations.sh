474b00d0dceefdc0429c6572f5ee9169008b30a0
kyryo <owenonions@gmail.com>
2021-01-07 07:54:42 +0200

    Trying with bundle exec

--- migrations.sh
+++ migrations.sh
@@ -1,5 +1,4 @@
 #!/bin/bashRun; 
 echo "migrating database....................."
 export PATH=$PATH:$HOME/bin:$HOME/.rvm/gems/ruby-2.5.3/bin:$HOME/.rvm/rubies/ruby-2.5.3/bin:$HOME/.gem/ruby/2.5.0/bin
-
-rake db:migrate
+bundle exec rake db:migrate
