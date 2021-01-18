b6a424fe605918a9c036259541891e614a2d82cf
kyryo <owenonions@gmail.com>
2021-01-18 14:21:35 +0200

    Adding tbl on data setup

--- migrations.sh
+++ migrations.sh
@@ -1,4 +1,2 @@
 #!/bin/bashRun; 
 echo "migrating database....................."
-export PATH=$PATH:$HOME/bin:$HOME/.rvm/gems/ruby-2.5.3/bin:$HOME/.rvm/rubies/ruby-2.5.3/bin:$HOME/.gem/ruby/2.5.0/bin:$HOME/.rvm/bin:/usr/local/sbin:/usr/local/bin
-bundle exec rake db:migrate
