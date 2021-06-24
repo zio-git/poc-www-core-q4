fe4f13777e6a0e9ffbee1e33815e4ea64f4be8a7
Kyryo <owenonions@gmail.com>
2021-06-24 08:29:43 +0200

    Added pushing functionality for core and art

--- art_shippingx.py
+++ art_shippingx.py
@@ -1,28 +1,30 @@
-import os
-import urllib.request
+import requests
 import json
 import platform
 import subprocess
+import os
 
-# define URL to capture data
-url = 'http://10.44.0.52/sites/api/v1/get_single_cluster/3'
-req = urllib.request.Request(url)
+def get_xi_data(url):
+    response = requests.get(url)
+    data = json.loads(response.text)
+    data = data[0]['fields']
+    return data
 
-# parsing response
-r = urllib.request.urlopen(req).read()
-xi_api = json.loads(r.decode('utf-8'))
+cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/3')
 
-all_sites_in_cluster = xi_api[0]['fields']['site']
+for site_id in cluster['site']:
+    site = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_site/' + str(site_id))
 
-for site_id in all_sites_in_cluster:
-    site_endpoint = "http://10.44.0.52/sites/api/v1/get_single_site/" + str(site_id)
-    site_req = urllib.request.Request(site_endpoint)
-    site_r = urllib.request.urlopen(site_req).read()
-    site_details = json.loads(site_r.decode('utf-8'))
-    sitex = site_details[0]['fields']
+    # lets check if the site is available
+    param = '-n' if platform.system().lower()=='windows' else '-c'
+    if subprocess.call(['ping', param, '1', site['ip_address']]) == 0:
+        
+        # ship data to remote site
+        push_art = "rsync " + "-r ssh $WORKSPACE/BHT-Core/apps-ART/ " + site['user'] + "@" + site['ip'] + ":~/var/www/BHT-Core/apps/ART"
+        os.system(push_art)
 
-    for site in xi_api['cluster']:
-        param = '-n' if platform.system().lower()=='windows' else '-c'
-        if subprocess.call(['ping', param, '1', site['ip']]) == 0:
-            push_art = "rsync " + "-r ssh $WORKSPACE/BHT-Core/apps-ART/ " + site['user'] + "@" + site['ip'] + ":~/var/www/BHT-Core/apps/ART"
-            os.system(push_art)
+
+
+
+
+
