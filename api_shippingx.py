92cffe03c6d36968c20e4075abbe55770553c140
Kyryo <owenonions@gmail.com>
2021-06-24 08:17:45 +0200

    Fixed shipping script

--- api_shippingx.py
+++ api_shippingx.py
@@ -1,30 +1,34 @@
-import os
-import urllib.request
+import requests
 import json
 import platform
 import subprocess
+import os
 
-# define URL to capture data
-url = 'http://10.44.0.52/sites/api/v1/get_single_cluster/3'
-req = urllib.request.Request(url)
+def get_xi_data(url):
+    response = requests.get(url)
+    data = json.loads(response.text)
+    data = data[0]['fields']
+    return data
 
-# parsing response
-r = urllib.request.urlopen(req).read()
-xi_api = json.loads(r.decode('utf-8'))
+cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/3')
 
-all_sites_in_cluster = xi_api[0]['fields']['site']
+for site_id in cluster['site']:
+    site = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_site/' + str(site_id))
 
-for site_id in all_sites_in_cluster:
-    site_endpoint = "http://10.44.0.52/sites/api/v1/get_single_site/" + str(site_id)
-    site_req = urllib.request.Request(site_endpoint)
-    site_r = urllib.request.urlopen(site_req).read()
-    site_details = json.loads(site_r.decode('utf-8'))
+    # lets check if the site is available
+    param = '-n' if platform.system().lower()=='windows' else '-c'
+    if subprocess.call(['ping', param, '1', site['ip_address']]) == 0:
+        
+        # ship data to remote site
+        push_api = "rsync " + "-r ssh $WORKSPACE/BHT-EMR-API/ " + site['username'] + "@" + site['ip_address'] + ":~/var/www/BHT-EMR-API"
+        os.system(push_api)
+        
+        # run setup script
+        run_api_script = "ssh " + site['username'] + "@" + site['ip_address'] + " 'cd /var/www/BHT-EMR-API && ./api_setup.sh"
+        os.system(run_api_script)
 
-    sitex = site_details[0]['fields']
-    for sitex in xi_api['cluster']:
-        param = '-n' if platform.system().lower()=='windows' else '-c'
-        if subprocess.call(['ping', param, '1', sitex['ip']]) == 0:
-            push_api = "rsync " + "-r ssh $WORKSPACE/BHT-EMR-API/ " + sitex['user'] + "@" + sitex['ip'] + ":~/var/www/BHT-EMR-API"
-            os.system(push_api)
-            run_api_script = "ssh " + sitex['user'] + "@" + sitex['ip'] + " 'cd /var/www/BHT-EMR-API && ./api_setup.sh"
-            os.system(run_api_script)
+
+
+
+
+
