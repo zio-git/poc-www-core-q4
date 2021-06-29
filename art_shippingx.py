138c80b358de567eff1fe883a0fb70dfc5818a60
Kyryo <owenonions@gmail.com>
2021-06-29 15:04:39 +0200

    Added re-try functionality for core

--- art_shippingx.py
+++ art_shippingx.py
@@ -15,17 +15,25 @@ cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/3')
 for site_id in cluster['site']:
     site = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_site/' + str(site_id))
 
-    # lets check if the site is available
-    param = '-n' if platform.system().lower()=='windows' else '-c'
-    if subprocess.call(['ping', param, '1', site['ip_address']]) == 0:
-        
-        # ship data to remote site
-        push_art = "rsync " + "-r $WORKSPACE/BHT-Core/apps/ART/ " + site['username'] + "@" + site['ip_address'] + ":/var/www/BHT-Core/apps"
-        os.system(push_art)
+    count = 0
 
-        # run setup script
-        run_api_script = "ssh " + site['username'] + "@" + site['ip_address'] + " 'cd /var/www/BHT-Core && ./core_art_setup.sh'"
-        os.system(run_api_script)
+    while(count < 3):
+
+        # lets check if the site is available
+        param = '-n' if platform.system().lower()=='windows' else '-c'
+        if subprocess.call(['ping', param, '1', site['ip_address']]) == 0:
+            
+            # ship data to remote site
+            push_art = "rsync " + "-r $WORKSPACE/BHT-Core/apps/ART/ " + site['username'] + "@" + site['ip_address'] + ":/var/www/BHT-Core/apps"
+            os.system(push_art)
+
+            # run setup script
+            run_api_script = "ssh " + site['username'] + "@" + site['ip_address'] + " 'cd /var/www/BHT-Core && ./core_art_setup.sh'"
+            os.system(run_api_script)
+
+            count = 3
+        else:
+            count = count + 1
 
 
 
