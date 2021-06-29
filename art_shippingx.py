d9a9bd924448363fcf66ac48abd042e917c08092
Kyryo <owenonions@gmail.com>
2021-06-29 15:09:33 +0200

    Adding re-try functionality for art

--- art_shippingx.py
+++ art_shippingx.py
@@ -15,6 +15,7 @@ cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/3')
 for site_id in cluster['site']:
     site = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_site/' + str(site_id))
 
+    # functionality for ping re-tries
     count = 0
 
     while(count < 3):
--- core_shippingx.py
+++ core_shippingx.py
@@ -21,18 +21,29 @@ cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/3')
 for site_id in cluster['site']:
     site = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_site/' + str(site_id))
 
-    #* lets check if the site is available
-    param = '-n' if platform.system().lower()=='windows' else '-c'
-    if subprocess.call(['ping', param, '1', site['ip_address']]) == 0:
-        
-        # shipping backup script
-        push_backup_script = "rsync " + "-r $WORKSPACE/devops_core_backup.sh " + site['username'] + "@" + site['ip_address'] + ":/var/www"
-        os.system(push_backup_script)
-        
-        # backing up application folder [Core & ART]
-        backup_script = "ssh " + site['username'] + "@" + site['ip_address'] + " 'cd /var/www && chmod 777 devops_core_backup.sh && ./devops_core_backup.sh'"
-        os.system(backup_script)
-        
-        #* ship data to remote site
-        push_core = "rsync " + "-r $WORKSPACE/BHT-Core " + site['username'] + "@" + site['ip_address'] + ":/var/www"
-        os.system(push_core)
+    # functionality for ping re-tries
+    count = 0
+
+    while(count < 3):
+
+        #* lets check if the site is available
+        param = '-n' if platform.system().lower()=='windows' else '-c'
+        if subprocess.call(['ping', param, '1', site['ip_address']]) == 0:
+            
+            # shipping backup script
+            push_backup_script = "rsync " + "-r $WORKSPACE/devops_core_backup.sh " + site['username'] + "@" + site['ip_address'] + ":/var/www"
+            os.system(push_backup_script)
+            
+            # backing up application folder [Core & ART]
+            backup_script = "ssh " + site['username'] + "@" + site['ip_address'] + " 'cd /var/www && chmod 777 devops_core_backup.sh && ./devops_core_backup.sh'"
+            os.system(backup_script)
+            
+            #* ship data to remote site
+            push_core = "rsync " + "-r $WORKSPACE/BHT-Core " + site['username'] + "@" + site['ip_address'] + ":/var/www"
+            os.system(push_core)
+
+            count = 3
+
+        else:
+
+            count = count + 1
