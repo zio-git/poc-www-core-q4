5f7a0079a67032761632da699b797837094c466c
zio-git <73478973+zio-git@users.noreply.github.com>
2021-07-27 22:08:10 +0200

    chiradzulu cluster

--- art_shippingx.py
+++ art_shippingx.py
@@ -28,9 +28,9 @@ def alert(url, params):
     return r
 
 
-recipients = ["+265998006237", "+265991450316", "+265995246144", "+265999611280", "+265888874484"]
+recipients = ["+265998006237", "+265991450316", "+265995246144", "+265999611280", "+265995440499"]
 
-cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/11')
+cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/12')
 
 for site_id in cluster['site']:
     site = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_site/' + str(site_id))
