748834d5563eecf7dd08262db099be6647823bff
zio-git <73478973+zio-git@users.noreply.github.com>
2021-07-27 18:35:42 +0200

    blantyre cluster

--- core_shippingx.py
+++ core_shippingx.py
@@ -27,10 +27,10 @@ def alert(url, params):
     r = requests.post(url, json=params, headers=headers)
     return r
 
-recipients = ["+265998006237", "+265991450316", "+265995246144"]
+recipients = ["+265998006237", "+265991450316", "+265995246144", "+265999611280", "+265992600883", "+265999679975"]
 
 #* Get cluster details
-cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/3')
+cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/5')
 
 for site_id in cluster['site']:
     site = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_site/' + str(site_id))
