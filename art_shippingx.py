6932c9934072eefeceffdf3a5e49805c3372b301
zio-git <73478973+zio-git@users.noreply.github.com>
2021-08-03 13:30:32 +0200

    matawale hc

--- art_shippingx.py
+++ art_shippingx.py
@@ -30,7 +30,7 @@ def alert(url, params):
 
 recipients = ["+265998006237", "+265991450316", "+265995246144", "+265992182669"] 
 
-cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/16')
+cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/14')
 
 for site_id in cluster['site']:
     site = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_site/' + str(site_id))
